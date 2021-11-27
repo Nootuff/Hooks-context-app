6743f9db184fda8d1beca1fb2b61d90c0a9bc793
Adam <adamwalkerlondon@gmail.com>
2021-11-27 10:15:19 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# An app using context built with hooks.
+
+Simple sign-in form that uses context to handle state, built with React Hooks.
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './styles/App.css';
+/*import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import PageContent from "./components/PageContent";
+import ThemeProvider from "./contexts/ThemeContext";
+import LanguageProvider  from "./contexts/LanguageContext";
+*/
+
+function App() {
+    return (
+      /*<ThemeProvider>
+        <LanguageProvider>
+         
+            <Navbar />
+            <Form />
+         
+        </LanguageProvider>
+      </ThemeProvider>
+      */
+     <div>
+       Tester hooks with context. 
+       </div>
+    );
+  }
+  
+  export default App;
